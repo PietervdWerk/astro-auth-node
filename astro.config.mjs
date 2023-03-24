81b88f950cf0e34b09e99b011806546ef40bd5cb
Pieter van der Werk <pieter.vdwerk@gmail.com>
2023-03-24 14:10:17 +0100

    Add authentication

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 import { defineConfig } from 'astro/config';
-
 import node from "@astrojs/node";
+import { loadEnv } from 'vite';
+import auth from '@pietervdwerk/auth-astro';
+import GitHub from '@auth/core/providers/github';
+const env = loadEnv('production', process.cwd(), '');
+
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: node({
-    mode: "standalone"
-  })
+    mode: 'standalone'
+  }),
+  integrations: [auth({
+    providers: [GitHub({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET
+    })]
+  })]
 });
